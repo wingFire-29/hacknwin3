1843788b4c4891e6c3944717838089092d676236
Parag <paragshrivastavadatia727@gmail.com>
2026-03-07 22:53:27 +0530

    mid commit

--- LawAi/LawAi/settings.py
+++ LawAi/LawAi/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.signing',
+    'apps.dashboard',
+    'apps.chatbot',
 ]
 
 MIDDLEWARE = [
--- LawAi/apps/chatbot/views.py
+++ LawAi/apps/chatbot/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+def chat(request):
+
+    if request.method == "POST":
+        
+        try:
+            data = json.loads(request.body)
+
+            user_message = data.get("message")
+
+            response = {
+                "message_received": user_message,
+                "reply": "Chatbot backend is working!"
+            }
+
+            return JsonResponse(response)
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)})
+
+    return JsonResponse({"error": "Only POST request allowed"})
